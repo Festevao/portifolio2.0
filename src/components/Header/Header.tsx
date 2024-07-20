9f23d0d000dd78c438942aec541d10db8e433b0d
Festevao <cadefm@hotmail.com>
2024-07-20 11:22:11 -0300

    feat/fix: header and footer style implement

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -66,7 +66,13 @@ const Header = () => {
   };
 
   return (
-    <header className='header relative flex flex-row w-full bg-black min-h-[70px] min-w-[250px] h-[9vh] text-white'>
+    <header
+      className='header relative flex flex-row w-full min-h-[70px] min-w-[250px] h-[9vh] text-white'
+      style={{
+        backgroundColor: '#000000',
+        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='409' height='49.1' viewBox='0 0 1000 120'%3E%3Cg fill='none' stroke='%23222' stroke-width='6.7' %3E%3Cpath d='M-500 75c0 0 125-30 250-30S0 75 0 75s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 45c0 0 125-30 250-30S0 45 0 45s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 105c0 0 125-30 250-30S0 105 0 105s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 15c0 0 125-30 250-30S0 15 0 15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500-15c0 0 125-30 250-30S0-15 0-15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 135c0 0 125-30 250-30S0 135 0 135s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3C/g%3E%3C/svg%3E")`
+      }}
+    >
       <div className='relative hidden lg:flex flex-row lg:gap-[2%] items-center justify-center w-full'>
         <RenderLinks />
       </div>
@@ -77,8 +83,8 @@ const Header = () => {
           </svg>
         </button>
       </div>
-      <div className={`fixed inset-0 z-50 bg-black bg-opacity-75 transition-opacity ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} onClick={closeMenu}></div>
-      <div className={`fixed top-0 left-0 h-full pt-[60px] bg-black w-64 transition-transform transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} z-50`} onClick={closeMenu}>
+      <div className={`fixed lg:hidden inset-0 z-50 bg-black bg-opacity-75 transition-opacity ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} onClick={closeMenu}></div>
+      <div className={`fixed lg:hidden top-0 left-0 h-full pt-[60px] bg-black w-64 transition-transform transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} z-50`} onClick={closeMenu}>
         <div className='flex flex-col p-4 gap-3'>
           <RenderLinks />
         </div>
