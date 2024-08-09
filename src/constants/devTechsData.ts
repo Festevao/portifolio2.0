61567275b7440d9cbcc0aa0ea0ba2596daa7b7cb
Festevao <cadefm@hotmail.com>
2024-08-09 16:35:32 -0300

    feat/fix: change tecnologies category from javasript to backend/frontend

--- src/constants/devTechsData.ts
+++ src/constants/devTechsData.ts
@@ -1,48 +1,48 @@
 export const devTechsData = [
   {
-    tags: ['Todos', 'JavaScript/TypeScript'],
+    tags: ['Todos', 'Backend'],
     name: 'NodeJS',
     description: 'Node.js é um ambiente de execução JavaScript que permite o desenvolvimento do lado do servidor. Utiliza um modelo de I/O não-bloqueante, orientado por eventos, que o torna leve e eficiente.',
     imgUrl: '/img/technologies/nodeJS.png',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript'],
+    tags: ['Todos', 'Frontend'],
     name: 'ReactJS',
     description: 'ReactJS é uma biblioteca JavaScript desenvolvida pelo Facebook para criar interfaces de usuário de forma eficiente. Utiliza um modelo baseado em componentes, onde cada parte da interface é encapsulada em um componente autônomo e reutilizável. Permite a integração com várias outras bibliotecas e frameworks do ecossistema JavaScript.',
     imgUrl: '/img/technologies/reactJS.webp',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript'],
+    tags: ['Todos', 'Backend'],
     name: 'NestJS',
     description: 'NestJS é um framework Node.js para construir aplicações eficientes, confiáveis e escaláveis. É muito prático para construção de API\'s escaláveis e microserviços.',
     imgUrl: '/img/technologies/nestJS.png',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript'],
+    tags: ['Todos', 'Frontend'],
     name: 'NextJS',
     description: 'Next.js é uma estrutura de desenvolvimento para React que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos, simplificando a criação de aplicações web rápidas.',
     imgUrl: '/img/technologies/nextJS.png',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript', 'Frameworks'],
+    tags: ['Todos', 'Backend', 'Frameworks'],
     name: 'Express',
     description: 'Express é um framework para Node.js, projetado para construir APIs e aplicações web de maneira rápida e minimalista. Fornece uma série de funcionalidades robustas para o desenvolvimento web (principalmente http).',
     imgUrl: '/img/technologies/expressJS.png',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript', 'Frameworks'],
+    tags: ['Todos', 'Backend', 'Frameworks'],
     name: 'NodeMailer',
     description: 'Nodemailer é um módulo para Node.js que facilita o envio de e-mails. Com suporte a vários protocolos de transporte de e-mail, torna a integração de funcionalidades de e-mail simples e eficiente.',
     imgUrl: '/img/technologies/nodemailer.webp',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript', 'Frameworks', 'Banco de dados'],
+    tags: ['Todos', 'Backend', 'Frameworks', 'Banco de dados'],
     name: 'PrismaORM',
     description: 'Prisma é um ORM (Object-Relational Mapping) para Node.js e TypeScript que oferece uma API de consulta baseada em GraphQL e gerador de tipos estáticos, facilitando a integração com bancos de dados relacionais. Suporta transações, migrações de esquema e otimizações automáticas.',
     imgUrl: '/img/technologies/prisma.png',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript', 'Frameworks', 'Banco de dados'],
+    tags: ['Todos', 'Backend', 'Frameworks', 'Banco de dados'],
     name: 'TypeORM',
     description: 'TypeORM é um ORM (Object-Relational Mapping) que suporta múltiplos bancos de dados e oferece recursos como relacionamentos avançados, lazy loading e migrações de esquema. Permite um controle granular sobre consultas SQL, sendo adequado para projetos que necessitam de maior flexibilidade no gerenciamento do banco de dados.',
     imgUrl: '/img/technologies/typeORM.png',
@@ -66,13 +66,13 @@ export const devTechsData = [
     imgUrl: '/img/technologies/swagger.webp',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript', 'Testes', 'Frameworks'],
+    tags: ['Todos', 'Backend', 'Testes', 'Frameworks'],
     name: 'Puppeteer',
     description: 'Puppeteer é uma biblioteca Node.js que fornece uma API de alto nível para controlar o navegador via DevTools Protocol. Utilizado principalmente para testes automatizados, captura de tela e scraping de sites, Puppeteer permite a interação programática com páginas web, incluindo a navegação, geração de PDFs e testes de desempenho. Sua capacidade de simular ações de usuários reais, como cliques e entradas de teclado, faz dele uma ferramenta poderosa.',
     imgUrl: '/img/technologies/puppeteer.png',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript'],
+    tags: ['Todos', 'Frontend'],
     name: 'HTML|CSS|JS',
     description: 'HTML, CSS e JavaScript são as três tecnologias fundamentais para o desenvolvimento web. HTML (HyperText Markup Language) estrutura o conteúdo da web, CSS (Cascading Style Sheets) estiliza e layouta essas estruturas, e JavaScript adiciona interatividade e comportamento dinâmico.',
     imgUrl: '/img/technologies/html_css_js.png',
@@ -108,7 +108,7 @@ export const devTechsData = [
     imgUrl: '/img/technologies/beekeeper.png',
   },
   {
-    tags: ['Todos', 'JavaScript/TypeScript', 'Frameworks'],
+    tags: ['Todos', 'Backend', 'Frameworks'],
     name: 'WhatsApp Web.js',
     description: 'WhatsApp Web.js é uma biblioteca JavaScript para interagir com o WhatsApp Web. Permite automação de mensagens, criação de bots e integração com outras aplicações usando a API do WhatsApp Web.',
     imgUrl: '/img/technologies/whatsapp-web.js.png',
@@ -132,130 +132,130 @@ export const devTechsData = [
     imgUrl: '/img/technologies/mongodb.png',
   },
   {
-    tags: ['Todos', 'Frameworks', 'JavaScript/TypeScript'],
+    tags: ['Todos', 'Frameworks', 'Frontend'],
     name: 'Svelte',
     description: 'Svelte é um framework moderno para construção de interfaces de usuário reativas. Ao invés de usar um virtual DOM, Svelte compila componentes para código altamente eficiente, resultando em aplicações mais rápidas e leves. Ideal para desenvolvedores que buscam simplicidade e desempenho otimizado no front-end.',
     imgUrl: '/img/technologies/svelte.svg',
   },
   {
-    tags: ["Todos", "Testes", "JavaScript/TypeScript"],
-    name: "Jest",
-    description: "Jest é um framework de testes em JavaScript, ideal para garantir a qualidade e funcionalidade do código com suporte a testes assíncronos e cobertura de código.",
-    imgUrl: "/img/technologies/jest.webp"
+    tags: ['Todos', 'Testes'],
+    name: 'Jest',
+    description: 'Jest é um framework de testes em JavaScript, ideal para garantir a qualidade e funcionalidade do código com suporte a testes assíncronos e cobertura de código.',
+    imgUrl: '/img/technologies/jest.webp'
   },
   {
-    tags: ["Todos", "JavaScript/TypeScript", "Frameworks"],
-    name: "handlebars",
-    description: "Handlebars é um motor de templates JavaScript que facilita a criação de views dinâmicas, usando templates limpos e sintaxe de mustaches.",
-    imgUrl: "/img/technologies/handlebars.png"
+    tags: ['Todos', 'Frameworks', 'Frontend'],
+    name: 'handlebars',
+    description: 'Handlebars é um motor de templates JavaScript que facilita a criação de views dinâmicas, usando templates limpos e sintaxe de mustaches.',
+    imgUrl: '/img/technologies/handlebars.png'
   },
   {
-    tags: ["Todos", "JavaScript/TypeScript", "Banco de dados"],
-    name: "Sequelize",
-    description: "Sequelize é um ORM para Node.js que facilita o gerenciamento de bancos de dados SQL, oferecendo suporte a transações, migrações e relacionamentos.",
-    imgUrl: "/img/technologies/sequelize.png"
+    tags: ['Todos', 'Banco de dados', 'Backend'],
+    name: 'Sequelize',
+    description: 'Sequelize é um ORM para Node.js que facilita o gerenciamento de bancos de dados SQL, oferecendo suporte a transações, migrações e relacionamentos.',
+    imgUrl: '/img/technologies/sequelize.png'
   },
   {
-    tags: ["Todos", "JavaScript/TypeScript", "Organização", "Frameworks"],
-    name: "class-validator",
-    description: "Class-validator é uma biblioteca que facilita a validação de objetos e classes em projetos TypeScript, garantindo integridade e consistência de dados.",
-    imgUrl: "/img/technologies/class-validator-transformer.png"
+    tags: ['Todos', 'Organização', 'Frameworks', 'Backend'],
+    name: 'class-validator',
+    description: 'Class-validator é uma biblioteca que facilita a validação de objetos e classes em projetos TypeScript, garantindo integridade e consistência de dados.',
+    imgUrl: '/img/technologies/class-validator-transformer.png'
   },
   {
-    tags: ["Todos", "JavaScript/TypeScript", "Frameworks"],
-    name: "axios",
-    description: "Axios é uma biblioteca JavaScript para realizar requisições HTTP, suportando promessas, interceptors e compatibilidade com navegadores e Node.js.",
-    imgUrl: "/img/technologies/axios.png"
+    tags: ['Todos', 'Frameworks', 'Backend'],
+    name: 'axios',
+    description: 'Axios é uma biblioteca JavaScript para realizar requisições HTTP, suportando promessas, interceptors e compatibilidade com navegadores e Node.js.',
+    imgUrl: '/img/technologies/axios.png'
   },
   {
-    tags: ["Todos", "Frameworks"],
-    name: "Django",
-    description: "Django é um framework web em Python que segue o padrão MVC, oferecendo uma solução robusta para desenvolvimento rápido de aplicações web seguras.",
-    imgUrl: "/img/technologies/django.png"
+    tags: ['Todos', 'Frameworks', 'Backend'],
+    name: 'Django',
+    description: 'Django é um framework web em Python que segue o padrão MVC, oferecendo uma solução robusta para desenvolvimento rápido de aplicações web seguras.',
+    imgUrl: '/img/technologies/django.png'
   },
   {
-    tags: ["Todos", "Mensageria", "Frameworks"],
-    name: "Celery",
-    description: "Celery é uma biblioteca Python para gerenciamento de filas de tarefas distribuídas, usada para processamento assíncrono em larga escala.",
-    imgUrl: "/img/technologies/celery.png"
+    tags: ['Todos', 'Mensageria', 'Frameworks', 'Backend'],
+    name: 'Celery',
+    description: 'Celery é uma biblioteca Python para gerenciamento de filas de tarefas distribuídas, usada para processamento assíncrono em larga escala.',
+    imgUrl: '/img/technologies/celery.png'
   },
   {
-    tags: ["Todos", "Frameworks"],
-    name: "Pandas",
-    description: "Pandas é uma biblioteca Python para análise e manipulação de dados, ideal para trabalhar com grandes volumes de dados tabulares.",
-    imgUrl: "/img/technologies/pandas.webp"
+    tags: ['Todos', 'Frameworks', 'Backend'],
+    name: 'Pandas',
+    description: 'Pandas é uma biblioteca Python para análise e manipulação de dados, ideal para trabalhar com grandes volumes de dados tabulares.',
+    imgUrl: '/img/technologies/pandas.webp'
   },
   {
-    tags: ["Todos", "Redes"],
-    name: "FreeNAS",
-    description: "FreeNAS é um sistema operacional para armazenamento em rede (NAS), permitindo compartilhar arquivos e gerenciar armazenamento com facilidade.",
-    imgUrl: "/img/technologies/freenas.png"
+    tags: ['Todos', 'Redes'],
+    name: 'FreeNAS',
+    description: 'FreeNAS é um sistema operacional para armazenamento em rede (NAS), permitindo compartilhar arquivos e gerenciar armazenamento com facilidade.',
+    imgUrl: '/img/technologies/freenas.png'
   },
   {
-    tags: ["Todos", "Redes", "Organização"],
-    name: "Active Directory",
-    description: "Active Directory é um serviço de diretório da Microsoft usado para gerenciar usuários, permissões e recursos em redes corporativas.",
-    imgUrl: "/img/technologies/active-directory.png"
+    tags: ['Todos', 'Redes', 'Organização'],
+    name: 'Active Directory',
+    description: 'Active Directory é um serviço de diretório da Microsoft usado para gerenciar usuários, permissões e recursos em redes corporativas.',
+    imgUrl: '/img/technologies/active-directory.png'
   },
   {
-    tags: ["Todos", "Redes", "Protocolo"],
-    name: "LDAP",
-    description: "LDAP é um protocolo para acessar e manter diretórios de informação distribuída em redes IP, utilizado em autenticação e controle de acesso.",
-    imgUrl: "/img/technologies/ldap.png"
+    tags: ['Todos', 'Redes', 'Protocolo'],
+    name: 'LDAP',
+    description: 'LDAP é um protocolo para acessar e manter diretórios de informação distribuída em redes IP, utilizado em autenticação e controle de acesso.',
+    imgUrl: '/img/technologies/ldap.png'
   },
   {
-    tags: ["Todos", "Monitoramento", "Redes"],
-    name: "Zabbix",
-    description: "Zabbix é uma plataforma de monitoramento para redes e servidores, oferecendo visualização de dados e alertas em tempo real.",
-    imgUrl: "/img/technologies/zabbix.png"
+    tags: ['Todos', 'Monitoramento', 'Redes'],
+    name: 'Zabbix',
+    description: 'Zabbix é uma plataforma de monitoramento para redes e servidores, oferecendo visualização de dados e alertas em tempo real.',
+    imgUrl: '/img/technologies/zabbix.png'
   },
   {
-    tags: ["Todos", "Frameworks"],
-    name: "WordPress",
-    description: "WordPress é uma plataforma CMS amplamente utilizada para criar e gerenciar sites e blogs, conhecida por sua flexibilidade e extensibilidade.",
-    imgUrl: "/img/technologies/wordpress.png"
+    tags: ['Todos', 'Frameworks', 'Backend', 'Frontend'],
+    name: 'WordPress',
+    description: 'WordPress é uma plataforma CMS amplamente utilizada para criar e gerenciar sites e blogs, conhecida por sua flexibilidade e extensibilidade.',
+    imgUrl: '/img/technologies/wordpress.png'
   },
   {
-    tags: ["Todos", "Hospedagem/Devops", "Redes"],
-    name: "Amazon EC2",
-    description: "Amazon EC2 é um serviço de computação na nuvem que permite criar e gerenciar instâncias de servidores virtuais escaláveis.",
-    imgUrl: "/img/technologies/aws-ec2.jpg"
+    tags: ['Todos', 'Hospedagem/Devops', 'Redes'],
+    name: 'Amazon EC2',
+    description: 'Amazon EC2 é um serviço de computação na nuvem que permite criar e gerenciar instâncias de servidores virtuais escaláveis.',
+    imgUrl: '/img/technologies/aws-ec2.jpg'
   },
   {
-    tags: ["Todos", "Banco de dados", "Hospedagem/Devops"],
-    name: "Amazon RDS",
-    description: "Amazon RDS facilita a configuração, operação e escalabilidade de bancos de dados na nuvem, suportando vários motores de banco de dados.",
-    imgUrl: "/img/technologies/aws-rds.png"
+    tags: ['Todos', 'Banco de dados', 'Hospedagem/Devops'],
+    name: 'Amazon RDS',
+    description: 'Amazon RDS facilita a configuração, operação e escalabilidade de bancos de dados na nuvem, suportando vários motores de banco de dados.',
+    imgUrl: '/img/technologies/aws-rds.png'
   },
   {
-    tags: ["Todos", "Redes"],
-    name: "Amazon VPC",
-    description: "Amazon VPC permite criar redes virtuais isoladas na AWS, oferecendo controle total sobre o ambiente de rede.",
-    imgUrl: "/img/technologies/aws-vpc.png"
+    tags: ['Todos', 'Redes'],
+    name: 'Amazon VPC',
+    description: 'Amazon VPC permite criar redes virtuais isoladas na AWS, oferecendo controle total sobre o ambiente de rede.',
+    imgUrl: '/img/technologies/aws-vpc.png'
   },
   {
-    tags: ["Todos", "Mensageria"],
-    name: "Amazon SQS",
-    description: "Amazon SQS é um serviço de enfileiramento de mensagens que facilita a comunicação entre componentes distribuídos de aplicações.",
-    imgUrl: "/img/technologies/aws-sqs.png"
+    tags: ['Todos', 'Mensageria'],
+    name: 'Amazon SQS',
+    description: 'Amazon SQS é um serviço de enfileiramento de mensagens que facilita a comunicação entre componentes distribuídos de aplicações.',
+    imgUrl: '/img/technologies/aws-sqs.png'
   },
   {
-    tags: ["Todos", "Hospedagem/Devops", "Monitoramento"],
-    name: "Amazon CloudWatch",
-    description: "Amazon CloudWatch monitora recursos da AWS e aplicações, fornecendo métricas e alarmes em tempo real.",
-    imgUrl: "/img/technologies/aws-cloudwatch.png"
+    tags: ['Todos', 'Hospedagem/Devops', 'Monitoramento'],
+    name: 'Amazon CloudWatch',
+    description: 'Amazon CloudWatch monitora recursos da AWS e aplicações, fornecendo métricas e alarmes em tempo real.',
+    imgUrl: '/img/technologies/aws-cloudwatch.png'
   },
   {
-    tags: ["Todos", "Redes", "Hospedagem/Devops"],
-    name: "Amazon S3",
-    description: "Amazon S3 é um serviço de armazenamento de objetos que permite armazenar e recuperar qualquer quantidade de dados a qualquer momento.",
-    imgUrl: "/img/technologies/aws-s3.png"
+    tags: ['Todos', 'Redes', 'Hospedagem/Devops'],
+    name: 'Amazon S3',
+    description: 'Amazon S3 é um serviço de armazenamento de objetos que permite armazenar e recuperar qualquer quantidade de dados a qualquer momento.',
+    imgUrl: '/img/technologies/aws-s3.png'
   },
   {
-    tags: ["Todos", "Hospedagem/Devops", "Organização"],
-    name: "AWS Lambda",
-    description: "AWS Lambda permite executar código em resposta a eventos sem a necessidade de gerenciar servidores, cobrando apenas pelo tempo de execução.",
-    imgUrl: "/img/technologies/aws-lambda.png"
+    tags: ['Todos', 'Hospedagem/Devops', 'Organização'],
+    name: 'AWS Lambda',
+    description: 'AWS Lambda permite executar código em resposta a eventos sem a necessidade de gerenciar servidores, cobrando apenas pelo tempo de execução.',
+    imgUrl: '/img/technologies/aws-lambda.png'
   },
 ];
 
@@ -270,8 +270,12 @@ export const devTechsOptions = [
     imgUrl: '/img/view-icon.png',
   },
   {
-    tag: 'JavaScript/TypeScript',
-    imgUrl: '/img/technologies/jsts.png',
+    tag: 'Backend',
+    imgUrl: '/img/technologies/backend.png',
+  },
+  {
+    tag: 'Frontend',
+    imgUrl: '/img/technologies/frontend.png',
   },
   {
     tag: 'Frameworks',
