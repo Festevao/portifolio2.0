55fc3074cf9a0feadac46a81bb2b5b329c29ceea
Festevao <cadefm@hotmail.com>
2024-08-11 21:45:24 -0300

    feat/fix: savepoint

--- src/components/CompanySection/CompanySection.tsx
+++ src/components/CompanySection/CompanySection.tsx
@@ -19,24 +19,24 @@ const CompanySection: React.FC<Props> = ({
     return jobs.map((job) => {
       return (
         <li key={`${companyName}-job-${job}`}>
-          - {job}
+          - {job}.
         </li>
       );
     });
   };
 
   return (
-    <div className='w-full flex flex-col shadow-lg items-center justify-center p-5 rounded-lg gap-5 border border-gray-200'>
+    <div className='w-full flex flex-col text-xs md:text-base shadow-lg items-center justify-center p-5 rounded-lg gap-5 border border-gray-200'>
       <h1 className='text-lg underline font-bold text-center'>{companyName}</h1>
       <div className='flex justify-center items-center w-[80%] sm:hidden'>
         <img className='w-full rounded-3xl shadow-lg' src={imgUrl} alt={companyName}/>
       </div>
-      <span>{description}</span>
+      <span className='text-center'>{description}.</span>
       <div className='w-full flex flex-row justify-center items-start flex-wrap sm:flex-nowrap'>
-        <div className='hidden justify-center items-center max-w-[30%] w-full min-h-[142px] min-w-[142px] sm:flex'>
+        <div className='hidden justify-center items-center max-w-[20%] w-full min-h-[142px] min-w-[142px] sm:flex'>
           <img className='w-full rounded-3xl shadow-lg' src={imgUrl} alt={companyName}/>
         </div>
-        <div className='h-full w-[4%] hidden sm:block'/>
+        <div className='h-full w-[20px] hidden sm:block'/>
         <div className='flex flex-col justify-start w-full gap-3 sm:mt-10'>
           <span><span className='underline font-bold'>Cargo:</span> <span>{jobsDescription}</span></span>
           <hr className='w-full'/>
--- src/constants/companies.ts
+++ src/constants/companies.ts
@@ -1,16 +1,52 @@
 export const companies = [
   {
     companyName: 'Transformando Faces (Clínica)',
-    jobs: ['Analista de redes.'],
+    jobs: [
+      'Manutenção de máquinas',
+      'Planejamento / implementação / monitoramento de infraestrutura de redes',
+      'Gestão de usuários em redes e servidores',
+      'Controle de acesso à recintos',
+      'Participção na definição de processos de TI',
+      'Desevolvimento de API',
+    ],
     jobsDescription: 'Suporte de TI => Analista de redes',
-    description: 'Uma empresa de armonização facial que opera no lifecenter e em são paulo',
+    description: `\ A "Transformando Faces" é uma clínica de harmonização facial reconhecida no Brasil e no mundo
+    \ pela excelência nos resultados e pelo cuidado com os clientes. Trabalhar lá foi uma experiência enriquecedora,
+    \ onde vi de perto como o trabalho de alta qualidade e precisão pode impactar positivamente a vida das pessoas.
+    \ Foi uma experiência que me mostrou como realmente a tecnologia deve ser usada para auxiliar os processos e pessoas`,
     imgUrl: '/img/companies/logo-tf.jfif',
   },
   {
     companyName: 'Transformando Faces (Cursos)',
-    jobs: ['Analista de redes.'],
+    jobs: [
+      'Planejamento de redes para eventos',
+      'Gestão de processos de tecnologia para eventos',
+      'Hospedagem de software',
+    ],
     jobsDescription: 'Suporte de TI => Analista de redes',
-    description: 'Uma empresa de armonização facial que opera no lifecenter e em são paulo',
+    description: `\ A empresa de cursos associada à "Transformando Faces" era um ambiente dinâmico e voltado para a
+    \ capacitação de profissionais na área de harmonização facial. Ali, tive a oportunidade de trabalhar diretamente
+    \ com a infraestrutura de redes e sistemas de triagem, garantindo que todas as necessidades técnicas fossem atendidas
+    \ durante a aplicação dos cursos. Foi uma experiência que me permitiu aplicar e expandir meu conhecimento em TI,
+    \ assegurando que os processos educacionais ocorressem sem interrupções.
+    \ Além disso, sempre tive desafios para adaptar a infraestrutura dos eventos dos cursos às condições dos locais alugados para
+    \ a ministração, "casando" as necessidades técnicas dos cursos com a infraestrutura já existente. Essa experiência
+    \ me ensinou a ser flexível e a encontrar soluções rápidas e eficientes para desafios inesperados, garantindo o
+    \ sucesso dos eventos e a satisfação dos participantes.`,
     imgUrl: '/img/companies/tf-curso-logo.png',
-  }
+  },
+  {
+    companyName: 'AjudaPet',
+    jobs: ['Desenvolvimento de software'],
+    jobsDescription: 'Desenvolvedor de software',
+    description: 'Uma startup de coleiras inteligentes pra pets.',
+    imgUrl: '/img/companies/ajudapet-logo.png',
+  },
+  {
+    companyName: 'DevsX',
+    jobs: ['Desenvolvimento de software'],
+    jobsDescription: 'Desenvolvedor de software',
+    description: 'Empresa de desolvimento de software.',
+    imgUrl: '/img/companies/devsx-logo.jpeg',
+  },
 ]
