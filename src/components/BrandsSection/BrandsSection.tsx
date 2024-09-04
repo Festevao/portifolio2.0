3cb32af96d6b0934d0587eba64cedc67c8efb948
Festevao <cadefm@hotmail.com>
2024-09-03 21:22:46 -0300

    Update BrandsSection.tsx

--- src/components/BrandsSection/BrandsSection.tsx
+++ src/components/BrandsSection/BrandsSection.tsx
@@ -9,16 +9,31 @@ interface Props {
 }
 
 const BrandsSection: React.FC<Props> = ({ brands }) => {
+  const [click, setClick] = useState<number | undefined>();
+
+  const handleClick = (index: number) => {
+    setClick(index);
+  }
+
   const RenderBrands = () => {
     return brands.map(({ name, title, description }, index) => (
       <div
         key={`brand-card-${index}`}
         className='group relative w-[100px] py-2 px-4 flex items-center justify-center bg-gray-300 rounded-md brand-card shadow-lg border-gray-400 border'
         title={title ?? name}
+        onClick={() => handleClick(index)}
+        onMouseOut={() => setClick(undefined)}
       >
         <input type='button' className='text-center whitespace-nowrap' value={name}/>
         <div
-          className='absolute bottom-full mb-2 w-max p-2 bg-black text-white text-sm rounded opacity-0 group-hover:opacity-100 group-focus:opacity-100 pointer-events-none'
+          className='absolute bottom-full mb-2 w-max p-2 bg-black text-white text-sm rounded opacity-0 group-hover:opacity-100 group-focus:opacity-100 pointer-events-none transition-opacity duration-300'
+          style={{
+            opacity: click != undefined
+              ? click === index
+                ? '1'
+                : '0'
+              : undefined,
+          }}
         >
           {description}
         </div>
