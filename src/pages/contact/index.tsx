357680c4c43163dd146b7574242f1fe13b4eff02
Festevao <cadefm@hotmail.com>
2024-09-04 00:45:58 -0300

    Update index.tsx

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -13,11 +13,13 @@ const Contact: NextPageWithLayout = () => {
   }, []);
 
   return (
-    <div className='w-[80%] flex flex-col'>
-      <h1 className='text-xl lg:text-2xl font-bold underline mb-5 mt-10 text-center'>Contato</h1>
-      <p className='text-center mb-10'>
-        Sinta-se a vontade para me contatar por qualquer um desses meios:
-      </p>
+    <div className='w-[80%] flex flex-col justify-around min-h-[60vh]'>
+      <div>
+        <h1 className='text-xl lg:text-2xl font-bold underline mb-5 mt-10 text-center'>Contato</h1>
+        <p className='text-center mb-10'>
+          Sinta-se a vontade para me contatar por qualquer um desses meios:
+        </p>
+      </div>
       <div className='w-full flex flex-wrap gap-5 items-center justify-center lg:justify-center mb-10'>
         <ContactCard href='https://www.linkedin.com/in/felipi-trindade-365a34143/' imgUrl='/img/contact/linkedin.png' title='Linkedin' contactString='Ver pefil'/>
         <ContactCard href='https://github.com/Festevao' imgUrl='/img/contact/github.png' title='Github' contactString='Ver pefil'/>
