478d682a2c0f9902408edde1e51226c90d94bfbd
Festevao <cadefm@hotmail.com>
2024-09-03 21:12:20 -0300

    Update BrandsSection.tsx

--- src/components/BrandsSection/BrandsSection.tsx
+++ src/components/BrandsSection/BrandsSection.tsx
@@ -9,25 +9,18 @@ interface Props {
 }
 
 const BrandsSection: React.FC<Props> = ({ brands }) => {
-  const [tooltipInfo, setTooltipInfo] = useState<string | undefined>("");
-
-  const handleMouseEnter = (description: string | undefined) => {
-    setTooltipInfo(description);
-  };
-
   const RenderBrands = () => {
     return brands.map(({ name, title, description }, index) => (
       <div
         key={`brand-card-${index}`}
         className='group relative w-[100px] py-2 px-4 flex items-center justify-center bg-gray-300 rounded-md brand-card shadow-lg border-gray-400 border'
         title={title ?? name}
-        onMouseEnter={() => handleMouseEnter(description)}
       >
-        <input type='button' className='group text-center whitespace-nowrap' value={name}/>
+        <input type='button' className='text-center whitespace-nowrap' value={name}/>
         <div
           className='absolute bottom-full mb-2 w-max p-2 bg-black text-white text-sm rounded opacity-0 group-hover:opacity-100 group-focus:opacity-100 pointer-events-none'
         >
-          {tooltipInfo}
+          {description}
         </div>
       </div>
     ));
