ab0df6c1369ce77156ac659e90844136d2970df3
Festevao <cadefm@hotmail.com>
2024-09-03 22:28:02 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        'black-to-light': 'linear-gradient(to right, #000000 0%, #4d4d4d 5%, #ffffff 30%, #ffffff 70%, #4d4d4d 95%, #000000 100%)',
+        'black-to-light': 'linear-gradient(to right, #000000 0%, #4d4d4d 3%, #ffffff 20%, #ffffff 80%, #4d4d4d 97%, #000000 100%)',
         'light-to-black': 'linear-gradient(to right, #ffffff 0%, #4d4d4d 30%, #000000 50%, #4d4d4d 70%, #ffffff 100%)',
       },
     },
